ccf61d46c099fce5bec9dc461fbd92d5f017afc8
Mike Cao <caochengjun369@gmail.com>
2023-01-23 10:52:04 -0500

    add work page

--- src/pages/WorkPage/WorkPage.jsx
+++ src/pages/WorkPage/WorkPage.jsx
@@ -3,63 +3,71 @@ import { Box, Flex } from "../../components/ds";
 import { Heading, Text } from "../../components/ds";
 export default function WorkPage() {
   return (
-    <Flex mt="120px" mx="48px" flexDirection="column">
-      <Flex alignItems="center">
-        <Heading.h2 color="#e85a4f" mr="8px">
+    <Flex
+      mt="120px"
+      mx="48px"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Flex justifyContent="center" alignItems="center">
+        <Heading.h1 color="#e85a4f" mr="16px">
           ActivateStaff -
-        </Heading.h2>
+        </Heading.h1>
         <a
           href="https://activatestaff.com/products/"
           target="_blank"
           rel="noreferrer"
         >
-          <p style={{ fontSize: 20, textDecoration: "underline" }}>
+          <p style={{ fontSize: 28, textDecoration: "underline" }}>
             Click to see Product Examples
           </p>
         </a>
       </Flex>
 
-      <Box mx="100px">
-        <Text.body1 fontWeight="bold">
-          Frontend Developer building and maintaining complex projects with
-          ownership across multiple products and features.
-        </Text.body1>
-      </Box>
-      <Flex>
-        <Box mx="24px">
-          <Text fontSize={[1, 2, 3]} fontWeight="bold" mb="24px">
-            Developed and maintained features on web apps (recruiter & client
-            portals) using React.js to provide thousands of staffing agency
-            clients with the most efficient workflow to make more placements and
-            build better relationships with candidates.
-          </Text>
-          <Box alignItems="center">
-            <img
-              mx="auto"
-              height="300px"
-              width="auto"
-              src="/images/web-app.png"
-              alt="web"
-            />
-          </Box>
-        </Box>
-
-        <Box mx="24px">
-          <Text fontSize={[1, 2, 3]} fontWeight="bold" mb="24px">
-            Developed and maintained features on mobile app (candidate app)
-            using React Native to give thousands of staffing candidates the best
-            experience they are looking for and help them find work, track time,
-            and engage with their agency with the app.
-          </Text>
-          <img
-            mx="auto"
-            height="300px"
-            width="auto"
-            src="/images/mobile-app.png"
-            alt="mobile"
-          />
-        </Box>
+      <Flex justifyContent="center" alignItems="center">
+        <Heading.h2>(Work Sample available upon request)</Heading.h2>
       </Flex>
+
+      <Box width={1 / 2}>
+        <ul>
+          <li>
+            <Heading.h3 mb="16px">
+              Implemented and maintained 20+ features on React web apps
+              (recruiter portals) and 10+ features on React Native mobile app
+              (candidate app), providing staffing agency clients and candidates
+              with the most efficient workflow and best work relationships.
+            </Heading.h3>
+          </li>
+          <li>
+            <Heading.h3 mb="16px">
+              Experienced in making Rest API calls to retrieve data from
+              endpoints and display it using React data table libraries.
+            </Heading.h3>
+          </li>
+          <li>
+            <Heading.h3 mb="16px">
+              Worked closely with product manager, UX designer and backend
+              developers to turn ideas into features in a timely manner.
+              Experienced in Figma and styled-components for better UI
+              implementation and clean code.
+            </Heading.h3>
+          </li>
+          <li>
+            <Heading.h3 mb="16px">
+              Experienced in using feature flags across web and mobile apps to
+              display targeted features to different staffing agencies on their
+              requests.
+            </Heading.h3>
+          </li>
+          <li>
+            <Heading.h3>
+              Contributed to company's frontend styleguides and jest
+              documentations.
+            </Heading.h3>
+          </li>
+        </ul>
+      </Box>
     </Flex>
   );
 }
